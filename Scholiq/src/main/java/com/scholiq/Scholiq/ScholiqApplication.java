3ff44a1eb4a4f130739dd3e9830a587f3689ec8d
MdMashum <40038774+mdmashum@users.noreply.github.com>
2024-07-13 14:02:01 +0530

    School front end +backend

---
+++ Scholiq/src/main/java/com/scholiq/Scholiq/ScholiqApplication.java
@@ -0,0 +1,13 @@
+package com.scholiq.Scholiq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScholiqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScholiqApplication.class, args);
+	}
+
+}
---
+++ Scholiq/src/test/java/com/scholiq/Scholiq/ScholiqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scholiq.Scholiq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScholiqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
